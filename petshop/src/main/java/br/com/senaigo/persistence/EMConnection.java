a2f52ae95c3bee483a732e2ff05edd3ac53f5bfc
aluno <->
2016-04-08 21:32:18 -0300

    entity manager

---
+++ petshop/src/main/java/br/com/senaigo/persistence/EMConnection.java
@@ -0,0 +1,23 @@
+package br.com.senaigo.persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EMConnection {
+	
+	public EMConnection(){}
+	
+	private static EntityManagerFactory em;
+	
+	private static EntityManagerFactory createEMF(){
+		if(em == null){
+			em = Persistence.createEntityManagerFactory("petshop");
+		}
+		return em;
+	}
+	
+	public static EntityManager createEM(){
+		return createEMF().createEntityManager();
+	}
+}
